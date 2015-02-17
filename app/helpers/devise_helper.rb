eac9332583b268a83ab6f9175a1e4c6247cd3c53
Sean Connelley <connelley@stamen.com>
2015-02-17 13:37:49 -0500

    Cleaning up indents in devise_helper

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,26 +1,27 @@
 module DeviseHelper
-   def devise_error_messages!
-       html = ""
+  # http://stackoverflow.com/questions/4101641/rails-devise-handling-devise-error-messages
+  def devise_error_messages!
+    html = ""
 
-       return html if resource.errors.empty?
+    return html if resource.errors.empty?
 
-       errors_number = 0
+    errors_number = 0
 
-       html << "<ul class=\"#{resource_name}_errors_list\">"
+    html << "<ul class=\"#{resource_name}_errors_list\">"
 
-       saved_key = ""
-       resource.errors.each do |key, value|
-         if key != saved_key
-           html << "<li class=\"#{key} error alert active\"> This #{key} #{value} </li>"
-           errors_number += 1
-         end
-         saved_key = key
-       end
-
-       unsolved_errors = pluralize(errors_number, "unsolved error")
-       html = "<h4 class=\"#{resource_name}_errors_title\"> You have #{unsolved_errors} </h4>" + html
-       html << "</ul>"
-
-       return html.html_safe
+    saved_key = ""
+    resource.errors.each do |key, value|
+      if key != saved_key
+        html << "<li class=\"#{key} error alert active\"> This #{key} #{value} </li>"
+        errors_number += 1
+      end
+      saved_key = key
     end
+
+    unsolved_errors = pluralize(errors_number, "unsolved error")
+    html = "<h4 class=\"#{resource_name}_errors_title\"> You have #{unsolved_errors} </h4>" + html
+    html << "</ul>"
+
+    return html.html_safe
+  end
 end
