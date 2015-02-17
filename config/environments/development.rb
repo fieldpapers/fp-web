7abf7ab74c41a58d5459eb792705886f814a5af3
Seth Fitzsimmons <seth@mojodna.net>
2015-02-17 14:13:17 -0800

    Disable asset request logging
    Convenient when using stdout for debugging.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+require "disable_assets_logger"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -43,4 +45,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.middleware.insert_before Rails::Rack::Logger, DisableAssetsLogger
 end
