c81f7b0c2585188bbef95e3c9241a932f7925ab8
Sean Connelley <connelley@stamen.com>
2015-02-25 10:59:50 -0500

    Helper method to determine active navbar element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  # Used to set the active navbar item
+  def current_nav_target
+    target = ""
+    if controller_name == 'compose'
+      target = 'compose'
+    elsif controller_name == 'snapshots' && action_name == 'new'
+      target = 'upload'
+    elsif controller_name == 'snapshots' || controller_name == 'atlases'
+      target = 'watch'
+    elsif controller_name == 'home' && action_name == 'advanced'
+      target = 'extend'
+    elsif controller_name == 'sessions'
+      target = 'sessions'
+    end
+    return target
+  end
+
 end
