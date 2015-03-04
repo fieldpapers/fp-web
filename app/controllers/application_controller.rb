77f4a5450212bf1364918f648680a2832308618d
Seth Fitzsimmons <seth@mojodna.net>
2015-03-04 11:03:16 -0800

    Explicit language selection (and persistence)
    Fixes fieldpapers/fieldpapers#6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  include HttpAcceptLanguage::AutoLocale
+  before_filter :set_locale
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -14,5 +14,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  def set_locale
+    cookies[:locale] = params[:locale] if params[:locale]
+
+    http_accept_language.user_preferred_languages.unshift cookies[:locale] if cookies[:locale]
+
+    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
   end
 end
