8a1b1c1d91843254eff9f431347fd00eafb7c76a
Sean Connelley <connelley@stamen.com>
2015-03-18 22:44:52 -0400

    Helpers to generate atlas and snapshot "person_href" for CSV
    Hidden field for `paper_size`

--- app/helpers/atlases_helper.rb
+++ app/helpers/atlases_helper.rb
@@ -1,2 +1,10 @@
 module AtlasesHelper
+
+  # Used in CSV output
+  def atlas_person_href(atlas)
+    if atlas.user_id
+      atlas_url(user: atlas.user_id)
+    end
+  end
+
 end
--- app/helpers/snapshots_helper.rb
+++ app/helpers/snapshots_helper.rb
@@ -1,2 +1,10 @@
 module SnapshotsHelper
+
+  # Used in CSV output
+  def snapshot_person_href(snapshot)
+    if snapshot.user_id
+      snapshot_url(user: snapshot.user_id)
+    end
+  end
+
 end
