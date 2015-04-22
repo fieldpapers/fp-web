e273fa78f055a3316cc14efba18b76a8ce597c33
Sean Connelley <connelley@stamen.com>
2015-04-22 09:54:03 -0400

    Exporting each page as GeoJSON feature, fieldpapers/fieldpapers#39
    Expanded feature metadata for GeoJSON export, fieldpapers/fieldpapers#39
    Corrected partial reference in page view

--- app/helpers/atlases_helper.rb
+++ app/helpers/atlases_helper.rb
@@ -1,6 +1,6 @@
 module AtlasesHelper
 
-  # Used in CSV output
+  # Used in CSV & GeoJSON output
   def atlas_person_href(atlas)
     if atlas.user_id
       atlas_url(user: atlas.user_id)
@@ -16,9 +16,21 @@ module AtlasesHelper
       type: 'Feature',
       properties: {
           type: 'atlas',
-          person_href: atlas_person_href(atlas),
-          href: atlas_url(atlas),
-          created: atlas.created_at.strftime('%a, %e %b %Y %H:%M:%S %z')
+          creator: atlas.creator_name,
+          title: atlas.title,
+          description: atlas.text,
+          providers: atlas.provider,
+          paper_size: atlas.paper_size,
+          orientation: atlas.orientation,
+          layout: atlas.layout,
+          zoom: atlas.zoom,
+          rows: atlas.rows,
+          cols: atlas.cols,
+          pages: atlas.atlas_pages,
+          created: atlas.created_at.strftime('%a, %e %b %Y %H:%M:%S %z'),
+          url: atlas_url(atlas),
+          url_pdf: atlas.pdf_url,
+          url_user: atlas_person_href(atlas),
       },
       geometry: {
           type: 'MultiPolygon',
@@ -40,6 +52,33 @@ module AtlasesHelper
     feature[:geometry][:coordinates] = polys
     out[:features].push(feature)
 
+    # create pages
+    atlas.pages.each do |page|
+      bbox = page.bbox
+
+      out[:features].push({
+        type: 'Feature',
+        properties: {
+            type: 'page',
+            provider: page.provider,
+            page_number: page.page_number,
+            zoom: page.zoom,
+            created: page.created_at.strftime('%a, %e %b %Y %H:%M:%S %z'),
+            url: atlas_url(atlas) + "/" + page.page_number,
+        },
+        geometry: {
+            type: 'Polygon',
+            coordinates: [[
+              [bbox[0],bbox[1]],
+              [bbox[0],bbox[3]],
+              [bbox[2],bbox[3]],
+              [bbox[2],bbox[1]],
+              [bbox[0],bbox[1]]
+            ]]
+        }
+      })
+    end
+
 
     # create snapshot features
     atlas.snapshots.each do |snapshot|
@@ -49,10 +88,20 @@ module AtlasesHelper
         type: 'Feature',
         properties: {
             type: 'snapshot',
-            person_href: snapshot_person_href(snapshot),
-            href: snapshot_url(snapshot),
-            atlas_page_href: atlas_url(atlas) + "/" + snapshot.page.page_number,
-            created: snapshot.created_at.strftime('%a, %e %b %Y %H:%M:%S %z')
+            title: snapshot.title,
+            description: snapshot.description,
+            uploader: snapshot.uploader_name,
+            created: snapshot.created_at.strftime('%a, %e %b %Y %H:%M:%S %z'),
+            min_row: snapshot.min_row,
+            max_row: snapshot.max_row,
+            min_column: snapshot.min_column,
+            max_column: snapshot.max_column,
+            min_zoom: snapshot.min_zoom,
+            max_zoom: snapshot.max_zoom,
+            base_url: snapshot.base_url,
+            url: snapshot_url(snapshot),
+            url_page: atlas_url(atlas) + "/" + snapshot.page.page_number,
+            url_uploader: snapshot_person_href(snapshot),
         },
         geometry: {
             type: 'Polygon',
