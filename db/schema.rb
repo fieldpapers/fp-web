c87bcde4e0be553cf9f18ea5e68194c18feee54e
Seth Fitzsimmons <seth@mojodna.net>
2015-04-30 16:26:59 -0700

    Populate snapshot bounds + zooms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430230647) do
+ActiveRecord::Schema.define(version: 20150430231207) do
 
   create_table "atlases", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
