7bdabeabf07ef11c459b202f740f52b263078ca4
Seth Fitzsimmons <seth@mojodna.net>
2015-05-01 16:49:35 -0700

    Reflect the updated tiler's DNS
    Refs fieldpapers/fieldpapers#70

--- config/initializers/fieldpapers.rb
+++ config/initializers/fieldpapers.rb
@@ -1,3 +1,3 @@
 module FieldPapers
-  TILE_BASE_URL = ENV["TILE_BASE_URL"] || "http://fieldpapers-tiler.herokuapp.com"
+  TILE_BASE_URL = ENV["TILE_BASE_URL"] || "http://tiles.next.fieldpapers.org"
 end
