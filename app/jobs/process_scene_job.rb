4f57c4528e7db75009ced5e43545923fe8dcce90
Seth Fitzsimmons <seth@mojodna.net>
2015-04-30 22:52:51 -0700

    Determine and populate extent and zoom

--- app/jobs/process_scene_job.rb
+++ app/jobs/process_scene_job.rb
@@ -38,13 +38,18 @@ class ProcessSceneJob < ActiveJob::Base
         print_href: url
 
       geotiff = process(snapshot, image)
+      extent = transform(get_extent(snapshot, geotiff))
 
       geotiff_url = upload(snapshot, geotiff)
 
       # TODO save geotiff_url vs. relying on conventions
-      # TODO update geojpeg_bounds
       snapshot.update \
         progress: 1,
+        west: extent[0][0],
+        south: extent[0][1],
+        east: extent[1][0],
+        north: extent[1][1],
+        zoom: page.zoom,
         base_url: "https://s3.amazonaws.com/#{BUCKET}/snapshots/#{snapshot.slug}", # TODO refactor
         has_geotiff: "yes", # TODO turn this into a boolean field
         decoded_at: Time.now
@@ -65,7 +70,35 @@ class ProcessSceneJob < ActiveJob::Base
 
     bucket = Rails.application.secrets.aws["s3_bucket_name"]
     s3_upload_url_data = Snapshot::S3_UPLOAD_URL_FORMAT.match(snapshot.s3_scene_url)
-    s3.buckets[bucket].objects[s3_upload_url_data[:path]]
+    s3.buckets[bucket].objects[s3_upload_url_data[:path]].read
+  end
+
+  def get_extent(snapshot, image)
+    cmd = %w(docker run --rm -i fieldpapers/paper gdalinfo /vsistdin/)
+
+    stdout, stderr, status = nil
+
+    begin
+      Timeout.timeout(30) do
+        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: image, binmode: true)
+      end
+    rescue Timeout::Error
+      Process.kill(9, status.pid)
+
+      raise "Timed out determining extent for snapshot #{snapshot.slug}"
+    end
+
+    raise "Failed to determine extent for snapshot #{snapshot.slug}\nstdout: #{stdout}\nstderr: #{stderr}" unless status.success?
+
+    stdout.lines.select do |x|
+        x =~ /(Lower Left|Upper Right)/
+      end.map(&:strip).map do |x|
+        x.gsub(/[^\(]*\((.+)\).+/, "\\1")
+      end.map do |x|
+        x.split(", ")
+      end.map do |x|
+        x.map(&:to_f)
+      end
   end
 
   def get_url(snapshot, image)
@@ -76,7 +109,7 @@ class ProcessSceneJob < ActiveJob::Base
 
     begin
       Timeout.timeout(30) do
-        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: image.read, binmode: true)
+        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: image, binmode: true)
       end
     rescue Timeout::Error
       Process.kill(9, status.pid)
@@ -84,9 +117,7 @@ class ProcessSceneJob < ActiveJob::Base
       raise "Timed out reading QR code for snapshot #{snapshot.slug}"
     end
 
-    unless status.success?
-      raise "Failed to read QR code for snapshot #{snapshot.slug}\nstdout: #{stdout}\nstderr: #{stderr}"
-    end
+    raise "Failed to read QR code for snapshot #{snapshot.slug}\nstdout: #{stdout}\nstderr: #{stderr}" unless status.success?
 
     update_progress(snapshot)
 
@@ -116,7 +147,7 @@ class ProcessSceneJob < ActiveJob::Base
 
     begin
       Timeout.timeout(60) do
-        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: image.read, binmode: true)
+        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: image, binmode: true)
       end
     rescue Timeout::Error
       Process.kill(9, status.pid)
@@ -124,13 +155,39 @@ class ProcessSceneJob < ActiveJob::Base
       raise "Timed out processing snapshot #{snapshot.slug}"
     end
 
-    unless status.success?
-      raise "Failed to process snapshot #{snapshot.slug}: #{stderr}"
-    end
+    raise "Failed to process snapshot #{snapshot.slug}: #{stderr}" unless status.success?
 
     update_progress(snapshot)
 
     stdout
+  end
+
+  def transform(coords)
+    coords = coords.map do |x|
+      x.join(" ")
+    end.join("\n")
+
+    cmd = %w(docker run --rm -i fieldpapers/paper gdaltransform -s_srs EPSG:3857 -t_srs EPSG:4326)
+
+    stdout, stderr, status = nil
+
+    begin
+      Timeout.timeout(30) do
+        (stdout, stderr, status) = Open3.capture3(*cmd, stdin_data: coords)
+      end
+    rescue Timeout::Error
+      Process.kill(9, status.pid)
+
+      raise "Timed out transforming coordinates: #{coords}"
+    end
+
+    raise "Failed to transform coordinates: #{coords}\nstdout: #{stdout}\nstderr: #{stderr}" unless status.success?
+
+    stdout.lines.map(&:strip).map do |x|
+      x.split(" ")[0..1]
+      end.map do |x|
+        x.map(&:to_f)
+      end
   end
 
   def update_progress(snapshot, increments = 1)
