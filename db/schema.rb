4bf4c4f11f66f9c58dae4e7a0e4d5a46ebb49cf8
Seth Fitzsimmons <seth@mojodna.net>
2015-05-27 23:06:00 -0700

    Sync schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519230420) do
+ActiveRecord::Schema.define(version: 20150528055308) do
 
   create_table "atlases", force: :cascade do |t|
     t.integer  "user_id",        limit: 4
