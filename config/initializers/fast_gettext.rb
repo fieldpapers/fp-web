5ea30bd9e72a32178a20cd09309beb7eaaf936d8
Seth Fitzsimmons <seth@mojodna.net>
2015-06-22 14:15:16 -0700

    Add missing locales
    Fixes fieldpapers/fieldpapers#170

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,5 +1,5 @@
 require 'gettext_i18n_rails/string_interpolate_fix'
 
 FastGettext.add_text_domain 'app', :path => 'locale', :type => :po
-FastGettext.default_available_locales = %w(da de en es fr id it ja nl pl pt tl)
+FastGettext.default_available_locales = %w(da de en es fr id it ja ku nl pl pt sw tl uk)
 FastGettext.default_text_domain = 'app'
