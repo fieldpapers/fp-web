9d8fbaa0e163ea7942a22ac3719fb999f206c541
Ian Ross <iross@cadasta.org>
2015-12-16 13:34:01 +0100

    Fix failing API tests
    Skip a couple when there are no valid AWS credentials (like on Travis);
    fix up AWS region to S3 URL mapping for another.

--- test/controllers/api/v1/s3_upload_controller_test.rb
+++ test/controllers/api/v1/s3_upload_controller_test.rb
@@ -2,8 +2,12 @@ module Api
   module V1
     class S3UploadControllerTest < ActionController::TestCase
       test "new snapshot upload info" do
-        get :show, filename: 'snap-1.png', format: :json
-        assert_response :success
+        begin
+          get :show, filename: 'snap-1.png', format: :json
+          assert_response :success
+        rescue AWS::Errors::MissingCredentialsError
+          skip "No AWS credentials"
+        end
       end
     end
   end
