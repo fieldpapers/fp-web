63b8387f5c55e56d3d47ed586faf4eeccf98efb0
Seth Fitzsimmons <seth@mojodna.net>
2016-01-29 17:38:35 -0800

    Externalize provider definitions

--- lib/providers.rb
+++ lib/providers.rb
@@ -1,5 +1,7 @@
 # Base maps available for Fieldpapers
 #
+require "json"
+
 class Providers
   # Use layers <key>
   def self.default
@@ -52,93 +54,11 @@ class Providers
     url
   end
 
-  # For layer options see: http://leafletjs.com/reference.html#tilelayer
-  # templates are (currently) expected to be ModestMaps-formatted, which means
-  # capital letters for placeholders
   def self.layers
-    {
-      'openstreetmap': {
-        label: 'OpenStreetMap',
-        template: 'http://{S}.tile.openstreetmap.org/{Z}/{X}/{Y}.png',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 19,
-      },
-      'toner': {
-        label: 'Black & White',
-        template: 'http://{S}.tile.stamen.com/toner-lite/{Z}/{X}/{Y}.png',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 20,
-      },
-      'satellite-labels': {
-        label: 'Satellite + Labels',
-        template: 'http://{S}.tile.stamen.com/boner/{Z}/{X}/{Y}.jpg',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 19,
-      },
-      'satellite-only': {
-        label: 'Satellite Only',
-        template: 'http://{S}.tile.stamen.com/bing-lite/{Z}/{X}/{Y}.jpg',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 19,
-      },
-      'humanitarian': {
-        label: 'Humanitarian',
-        template: 'http://{S}.tile.openstreetmap.fr/hot/{Z}/{X}/{Y}.png',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 20,
-      },
-      'humanitarian-nepal': {
-        label: 'Humanitarian-Nepal',
-        template: 'https://{S}.tiles.mapbox.com/v4/kll.ptthjjor/{Z}/{X}/{Y}@2x.png?access_token=pk.eyJ1Ijoia2xsIiwiYSI6IktVRUtfQnMifQ.GJAHJPvusgK_f0NsSXS8QA',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 20,
-      },
-      'mapbox-satellite': {
-        label: 'Mapbox Satellite',
-        template: 'http://{S}.tiles.mapbox.com/v3/stamen.i808gmk6/{Z}/{X}/{Y}.png',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 19,
-      },
-      'opencyclemap': {
-        label: 'OpenCycleMap',
-        template: 'http://{S}.tile.opencyclemap.org/cycle/{Z}/{X}/{Y}.png',
-        options: {
-          attribution: ''
-        },
-        minzoom: 0,
-        maxzoom: 20,
-      },
-      'sputnik.ru': {
-        label: 'Sputnik.RU',
-        template: 'http://{S}.tiles.maps.sputnik.ru/{z}/{x}/{y}.png',
-        options: {
-          attribution: ' © <a href="http://sputnik.ru">Спутник</a> | © <a href="http://www.openstreetmap.org/copyright">Openstreetmap</a> | © <a href="http://www.naturalearthdata.com/about/terms-of-use/">Natural Earth</a> '
-        },
-        minzoom: 0,
-        maxzoom: 19,
-      },
-
-    }
+    # For layer options see: http://leafletjs.com/reference.html#tilelayer
+    # templates are (currently) expected to be ModestMaps-formatted, which means
+    # capital letters for placeholders
+    @layers ||= JSON.parse(File.read("config/providers.json"))
   end
+
 end
