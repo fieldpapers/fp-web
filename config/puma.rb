91253a5861291a6a5c0486e586c0bfce4bd46f81
Seth Fitzsimmons <seth@mojodna.net>
2016-05-27 14:47:04 -0700

    Have puma respect RAILS_ENV

--- config/puma.rb
+++ config/puma.rb
@@ -8,7 +8,7 @@ preload_app!
 
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || ENV['RAILS_ENV'] || 'development'
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
