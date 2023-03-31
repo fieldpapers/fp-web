d8f8ebc5a567b2d1cb4b8ef515443c1caaf377a2
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2023-03-31 13:03:12 -0400

    Drop support for broken, unused IAM EC2 login

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,36 +5,15 @@ when "local"
     path: "#{FieldPapers::STATIC_PATH}:url",
   )
 when "s3"
-  if Rails.application.secrets[:aws][:access_key_id] && Rails.application.secrets[:aws][:secret_access_key]
-    Paperclip::Attachment.default_options.merge!(
-      storage:              :s3,
-      s3_credentials: {
-        access_key_id:      Rails.application.secrets[:aws][:access_key_id],
-        secret_access_key:  Rails.application.secrets[:aws][:secret_access_key],
-        bucket:             Rails.application.secrets[:aws][:s3_bucket_name]
-      },
-      s3_region: Rails.application.secrets[:aws][:s3_bucket_region],
-      s3_permissions:       :private,
-      s3_protocol:          'https'
-    )
-  else
-    Paperclip::Attachment.default_options.merge!(
-      storage:              :s3,
-
-      # If no AWS credentials were provided, assume we're running on EC2 with an
-      # IAM role.
-      s3_credentials: lambda { |a|
-        prov = Aws::Core::CredentialProviders::EC2Provider.new
-
-        return {
-          bucket: Rails.application.secrets[:aws][:s3_bucket_name],
-          access_key_id: prov.credentials[:access_key_id],
-          secret_access_key: prov.credentials[:secret_access_key]
-        }
-      },
-      s3_region: Rails.application.secrets[:aws][:s3_bucket_region],
-      s3_permissions:       :private,
-      s3_protocol:          'https'
-    )
-  end
+  Paperclip::Attachment.default_options.merge!(
+    storage:              :s3,
+    s3_credentials: {
+      access_key_id:      Rails.application.secrets[:aws][:access_key_id],
+      secret_access_key:  Rails.application.secrets[:aws][:secret_access_key],
+      bucket:             Rails.application.secrets[:aws][:s3_bucket_name]
+    },
+    s3_region: Rails.application.secrets[:aws][:s3_bucket_region],
+    s3_permissions:       :private,
+    s3_protocol:          'https'
+  )
 end
--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,18 +1,6 @@
 S3DirectUpload.config do |c|
-  if Rails.application.secrets[:aws][:access_key_id] && Rails.application.secrets[:aws][:secret_access_key]
-    # if AWS credentials are available, use them
-
-    c.access_key_id     = Rails.application.secrets[:aws][:access_key_id]
-    c.secret_access_key = Rails.application.secrets[:aws][:secret_access_key]
-  else
-    # if AWS credentials are _not_ available, assume we're on EC2 with an IAM role
-    prov = Aws::Core::CredentialProviders::EC2Provider.new
-
-    c.access_key_id     = lambda { prov.credentials[:access_key_id] }
-    c.secret_access_key = lambda { prov.credentials[:secret_access_key] }
-    c.session_token = lambda { prov.credentials[:session_token] }
-  end
-
+  c.access_key_id     = Rails.application.secrets[:aws][:access_key_id]
+  c.secret_access_key = Rails.application.secrets[:aws][:secret_access_key]
   c.bucket = Rails.application.secrets[:aws][:s3_bucket_name]
   c.region = Rails.application.secrets[:aws][:s3_bucket_region]
   c.url = "https://s3.#{c.region}.amazonaws.com/#{c.bucket}/"
