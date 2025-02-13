26ddd87445c744afebc1b2742c1292dcf3bbf506
Quincy Morgan <2046746+quincylvania@users.noreply.github.com>
2025-02-12 21:35:16 -0500

    Sync up supported langs

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,6 +1,6 @@
 require 'gettext_i18n_rails/string_interpolate_fix'
 
 FastGettext.add_text_domain('app', path: 'locale', type: :po)
-FastGettext.default_available_locales = %w(da de en es es_MX fr id it ja ku nl pl pt ru sw tl uk vi zh_CN zh_TW)
+FastGettext.default_available_locales = %w(ar cs da de en es es_MX fr hu id it ja ku nl pl pt pt_BR ru sw tl uk vi zh_CN zh_TW)
 FastGettext.default_text_domain = 'app'
 FastGettext.default_locale = 'en'
