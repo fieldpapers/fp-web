595fa16df48579ac477dd3008de6d6f6f855bae3
Jake Low <hello@jakelow.com>
2026-09-21 22:57:08 -0700

    Use info log level by default

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,9 +47,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
-  # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
+  # Set this env var to 'debug' to log every SQL statement. Useful when you
+  # need it, but disabled by default since it increases the log volume a ton
+  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info").to_sym
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
